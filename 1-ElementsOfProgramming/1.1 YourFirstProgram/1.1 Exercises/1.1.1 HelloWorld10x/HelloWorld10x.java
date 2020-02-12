d846b45988ed021bf04123f7c1cc942934feddbd
MarcusPlieninger <mwp2101@columbia.edu>
2020-02-12 06:52:16 -0500

    Add 1.1 exercises

---
+++ 1-ElementsOfProgramming/1.1 YourFirstProgram/1.1 Exercises/1.1.1 HelloWorld10x/HelloWorld10x.java
@@ -0,0 +1,15 @@
+public class HelloWorld10x {
+    public static void main(String[] args) {
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+    }
+}
---
+++ 1-ElementsOfProgramming/1.1 YourFirstProgram/1.1 Exercises/1.1.6 UseThree/UseThree.java
@@ -0,0 +1,11 @@
+public class UseThree {
+    public static void main(String[] args) {
+        System.out.print("Hi ");
+        System.out.print(args[0]);
+        System.out.print(", ");
+        System.out.print(args[1]);
+        System.out.print(", and ");
+        System.out.print(args[2]);
+        System.out.println(".");
+    }
+}
