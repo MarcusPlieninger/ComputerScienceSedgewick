dc05aa85fae8a769a1d01cf4c97d36604b0643af
MarcusPlieninger <mwp2101@columbia.edu>
2020-02-13 07:53:01 -0500

    Rename

---
+++ 1-ElementsOfProgramming/1.1 YourFirstProgram/1.1 Exercises/1.1.1 TenHelloWorlds/TenHelloWorlds.java
@@ -0,0 +1,15 @@
+public class TenHelloWorlds {
+    public static void main(String[] args) {
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+    }
+}
