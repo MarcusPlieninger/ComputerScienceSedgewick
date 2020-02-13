bbfa33e2f06d10ad1c001d9b69764e4cec4cd999
MarcusPlieninger <mwp2101@columbia.edu>
2020-02-13 07:53:55 -0500

    Add 1.2.2 examples

---
+++ 1-ElementsOfProgramming/1.2 Built-inTypesofData/1.2 Examples/1.2.2 IntOps/IntOps.java
@@ -0,0 +1,12 @@
+public class IntOps {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int p = a * b;
+        int q = a / b;
+        int r = a % b;
+        System.out.println(a+"*"+"b"+"="+p);
+        System.out.println(a+"/"+"b"+"="+q);
+        System.out.println(a+"%"+"b"+"="+r);
+    }
+}
